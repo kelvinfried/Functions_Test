b0f95ba67424f405551117544de3ef30982eef51
Kelvin <Kelvinfriedchicken@gmail.com>
2021-12-07 11:06:00 +0000

    Starting to develop the function ofr cooking any generic food item

---
+++ Function.cpp
@@ -0,0 +1,31 @@
+//Function testing
+#include <iostream>
+
+//Program Functions
+void cook_food()
+{
+	std::cout<< "We will be cooking the food item " << std::endl;
+}
+
+void cook_dinner()
+{
+	std::cout<< "" << std::endl;
+	std::cout<< "Cooking food function" << std::endl;
+}
+
+int main()
+{
+	//impliment 5 functions based on recipies 
+	
+	//Cook Dinner Function
+	std::cout<< "Cook Dinner functions" << std::endl;
+	
+		//Cook dinner will be used to call these functions
+	std::cout<< "Cook Steak" << std::endl;
+	std::cout<< "Cook Omelette" << std::endl;
+	std::cout<< "Cook Mushrooms" << std::endl;
+	std::cout<< "Cook Peas" << std::endl;
+	std::cout<< "Cook Peas" << std::endl;
+	
+	return 0;
+}
