ff5b1b56874ee3d4dc8597076c07bfcb4b564b21
Kelvin <Kelvinfriedchicken@gmail.com>
2021-12-09 22:59:23 +0000

    Commented out step 8 outputs

--- Function.cpp
+++ Function.cpp
@@ -214,9 +214,11 @@ int main()
 	//std::cout<< "If a and a are alphabetical " << alphabetical( 'a', 'a' ) << std::endl << "it will return 1." << std::endl; //As expected returns false
 	
 	//step 8
-	std::cout<< "The first value is 0' and it's radiant is " << deg_to_rad( 0.0f ) << std::endl;	//Working fine
-	std::cout<< "The first value is 30' and it's radiant is " << deg_to_rad( 30.0f ) << std::endl;	//working fine
-	std::cout<< "The first value is 120' and it's radiant is " << deg_to_rad( 120.0f ) << std::endl;	//working fine
+	//std::cout<< "The first value is 0' and it's radiant is " << deg_to_rad( 0.0f ) << std::endl;	//Working fine
+	//std::cout<< "The first value is 30' and it's radiant is " << deg_to_rad( 30.0f ) << std::endl;	//working fine
+	//std::cout<< "The first value is 120' and it's radiant is " << deg_to_rad( 120.0f ) << std::endl;	//working fine
+	
+	//Step 9
 	
 	return 0;
 }
